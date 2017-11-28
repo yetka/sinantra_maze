79c25d6e83f828a31fa89f52e9ec6dd45e05d9d5
Malgorzata Haniszewska <gosia.haniszewska@gmail.com>
2017-11-28 15:39:42 -0800

    add rules for room 2

--- lib/maze.rb
+++ lib/maze.rb
@@ -5,8 +5,20 @@ class Maze
   end
 
   def check_move
-    if ((@room == 1) & (@direction == "W"))
-      "You are in room 3"
+    if (@room == 1)
+      if @direction == "W"
+        "You are in room 2"
+      elsif @direction == "N"
+        "You are in room 3"
+      else
+        "This direction is not available"
+      end
+    elsif (@room == 2)
+      if @direction == "E"
+        "You are in room 1"
+      else
+        "This direction is not available"
+      end
     end
   end
 end
--- spec/maze_spec.rb
+++ spec/maze_spec.rb
@@ -6,7 +6,15 @@ describe(Maze) do
 
     it("returns you are in a room 2 if user chooses W in room 1") do
       new_maze = Maze.new(1,"W")
+      expect(new_maze.check_move()).to(eq("You are in room 2"))
+    end
+    it("returns you are in a room 3 if user chooses N in room 1") do
+      new_maze = Maze.new(1,"N")
       expect(new_maze.check_move()).to(eq("You are in room 3"))
+    end
+    it("returns you are in a room 1 if user chooses E in room 2") do
+      new_maze = Maze.new(2,"E")
+      expect(new_maze.check_move()).to(eq("You are in room 1"))
     end
 
   end
